57f0390e43fb9827c9f329bbe5edf6d492e76acb
Firman363 <jusles363@gmail.com>
2019-07-07 09:48:02 +0700

    penyeleksian player

--- ajax/updateDataBlack1.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('connnection.php');
-mysqli_query($conn, 'UPDATE dataPlayer SET player = 1 WHERE name = "black"');
-echo"gfh";
-?>
--- ajax/updateDataBlack2.php
+++
@@ -1,4 +0,0 @@
-<?php
-include('../asset/php/connnection.php');
-mysqli_query($conn, 'UPDATE dataPlayer SET player = 2 WHERE name = "black"');
-?>
